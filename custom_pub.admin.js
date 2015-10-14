e38723491a9204322ab25e0cb4ab69151486e3f9
Dan Boulet <dan@danboulet.com>
2015-10-14 15:56:13 -0500

    Fix JavaScript lint issue: fix use of undefined variables.

--- custom_pub.admin.js
+++ custom_pub.admin.js
@@ -5,13 +5,13 @@
       $("td.custom_pub-option-edit-cell").html('<a href="#">Edit</a>').css("text-align", "right");
       $("tr.custom_pub-form-edit").hide();
       $("td.custom_pub-option-edit-cell > a").bind('click', function () {
-        $this = $(this);
-        $opt = [];
-        $opt["Edit"] = "Close";
-        $opt["Close"] = "Edit";
+        var $this = $(this);
+        var opt = [];
+        var txt = $this.text();
+        opt["Edit"] = "Close";
+        opt["Close"] = "Edit";
         $this.parents('tr.custom_pub-option-row').next("tr.custom_pub-form-edit").toggle();
-        $txt = $this.text();
-        $this.text($opt[$txt]);
+        $this.text(opt[txt]);
         return false;
       });
       $("th.close-custom-pub-table").html('<a>X</a>')
@@ -19,15 +19,15 @@
       $("th.close-custom-pub-table > a")
       .css('cursor', 'pointer')
       .live('click', function () {
-        $this = $(this);
-        $opt = [];
-        $opt["Edit"] = "Close";
-        $opt["Close"] = "Edit";
-        $row = $this.parents("tr.custom_pub-form-edit");
+        var $this = $(this);
+        var opt = [];
+        var $row = $this.parents("tr.custom_pub-form-edit");
+        var $link = $row.prev('tr.custom_pub-option-row').find("td.custom_pub-option-edit-cell > a");
+        var txt = $link.text();
+        opt["Edit"] = "Close";
+        opt["Close"] = "Edit";
         $row.toggle();
-        $link = $row.prev('tr.custom_pub-option-row').find("td.custom_pub-option-edit-cell > a");
-        $txt = $link.text();
-        $link.text($opt[$txt]);
+        $link.text(opt[txt]);
         return false;
       });
     }
